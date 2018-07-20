ecc01a3550adf739a66c295e5657f5a29b1a1fc8
Turbanov Valeriy <tur.4000@gmail.com>
2018-07-20 13:20:15 +0300

    Добавляем html

--- app/routes.py
+++ app/routes.py
@@ -3,4 +3,14 @@ from app import app
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    user = {'username': 'Вася'}
+    return '''
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Домашняя страница - Microblog</title>
+    </head>
+    <body>
+        <h1>Hello, ''' + user['username'] + '''!</h1>
+    </body>
+</html>'''
