0fc4021c12d039598db63fed375f1d91c3d14b81
Turbanov Valeriy <tur.4000@gmail.com>
2018-07-20 13:32:38 +0300

    Подключен механизм шаблонов html

--- app/routes.py
+++ app/routes.py
@@ -1,16 +1,8 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
     user = {'username': 'Вася'}
-    return '''
-<!DOCTYPE html>
-<html>
-    <head>
-        <title>Домашняя страница - Microblog</title>
-    </head>
-    <body>
-        <h1>Hello, ''' + user['username'] + '''!</h1>
-    </body>
-</html>'''
+    return render_template('index.html', title='Home', user=user)
