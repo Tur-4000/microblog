bb64929a87cca2fa4c50eefe8dbb0ef58848c480
Tur.4000 <tur.4000@gmail.com>
2018-07-22 00:53:56 +0300

    Более интересные профили

--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,17 @@
+from datetime import datetime
 from flask import render_template, flash, redirect, url_for, request
 from app import app, db
 from app.forms import LoginForm, RegistrationForm
 from flask_login import current_user, login_user, login_required, logout_user
 from app.models import User
 from werkzeug.urls import url_parse
+
+@app.before_request
+def before_request():
+    if current_user.is_authenticated:
+        current_user.last_seen = datetime.utcnow()
+        db.session.commit()
+
 
 @app.route('/')
 @app.route('/index')
