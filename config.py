6a2614cc12953853973a00340d9c57b62b466506
Turbanov Valeriy <tur.4000@gmail.com>
2018-07-25 17:36:52 +0300

    email

--- config.py
+++ config.py
@@ -9,7 +9,7 @@ class Config(object):
     MAIL_SERVER = os.environ.get('MAIL_SERVER') or 'localhost'
     MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
     MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
-    MAIL_USERNAME = os.environ.get('MAIL_USERNAME') or 'flask'
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME') or 'flask@localhost'
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD') or '111'
     ADMINS = ['admin@localhost']
     POSTS_PER_PAGE = 10
