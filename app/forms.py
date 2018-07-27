98660e0a6641a64378402a566b003e5df2d738aa
Turbanov <tur.4000@gmail.com>
2018-07-28 01:06:43 +0300

    добавлено редактирование email пользователя

--- app/forms.py
+++ app/forms.py
@@ -30,18 +30,26 @@ class RegistrationForm(FlaskForm):
 
 class EditProfileForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(), Email()])
     about_me = TextAreaField('About me', validators=[Length(min=0, max=140)])
     submit = SubmitField('Submit')
     
-    def __init__(self, original_username, *args, **kwargs):
+    def __init__(self, original_username, original_email, *args, **kwargs):
         super(EditProfileForm, self).__init__(*args, **kwargs)
         self.original_username = original_username
+        self.original_email = original_email
 
     def validate_username(self, username):
         if username.data != self.original_username:
             user = User.query.filter_by(username=self.username.data).first()
             if user is not None:
-                raise ValidationError('Please use a different username')
+                raise ValidationError('Пожалуйста, выберите другое имя')
+    
+    def validate_email(self, email):
+        if email.data != self.original_email:
+            email = User.query.filter_by(email=self.email.data).first()
+            if email is not None:
+                raise ValidationError('Такой адрес уже используется')
 
 
 class PostForm(FlaskForm):
