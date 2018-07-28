2a87ddaa4f0e42e73a04c993e2651c90a289ecb3
Turbanov <tur.4000@gmail.com>
2018-07-29 00:33:22 +0300

    Исправление опечаток

--- app/__init__.py
+++ app/__init__.py
@@ -9,7 +9,7 @@ from flask_login import LoginManager
 from flask_mail import Mail
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
-from flask_babel import Babel
+from flask_babel import Babel, lazy_gettext as _l
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -54,6 +54,6 @@ if not app.debug:
 
 @babel.localeselector
 def get_locale():
-    return request.accept_laguages.best_match(app.config['LANGUAGES'])
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 from app import routes, models, errors
