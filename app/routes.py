fff0e5510320e5ddb481c9f99ea7d7fa2a0eccdd
Turbanov <tur.4000@gmail.com>
2018-07-29 00:41:04 +0300

    Добавлена локализация отображения даты/времени

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-from flask import render_template, flash, redirect, url_for, request
+from flask import render_template, flash, redirect, url_for, request, g
 from app import app, db
 from app.forms import LoginForm, RegistrationForm, EditProfileForm, PostForm, ResetPasswordRequestForm, ResetPasswordForm
 from flask_login import current_user, login_user, login_required, logout_user
@@ -13,6 +13,7 @@ def before_request():
     if current_user.is_authenticated:
         current_user.last_seen = datetime.utcnow()
         db.session.commit()
+    g.locale = str(get_locale())
 
 
 @app.route('/', methods=['GET', 'POST'])
