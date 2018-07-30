facde0079bf913f42d6b91ec826e22549a33f01b
Turbanov <tur.4000@gmail.com>
2018-07-31 00:55:59 +0300

    Форматирование

--- app/email.py
+++ app/email.py
@@ -14,4 +14,4 @@ def send_email(subject, sender, recipients, text_body, html_body):
     msg.body = text_body
     msg.html = html_body
     Thread(target=send_async_email,
-args=(current_app._get_current_object(), msg)).start()
+           args=(current_app._get_current_object(), msg)).start()
